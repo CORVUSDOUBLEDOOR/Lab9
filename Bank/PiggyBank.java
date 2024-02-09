c2796ff356d945d7fdeaaddfa358bdbe2adaad88
CORVUS-DOUBLEDOOR <153846979+CORVUSDOUBLEDOOR@users.noreply.github.com>
2024-02-09 16:27:21 +0700

    Update PiggyBank.java

--- Bank/PiggyBank.java
+++ Bank/PiggyBank.java
@@ -7,31 +7,19 @@ public class PiggyBank implements Measurable{
     private int size;
 
     public void addOne(int n) {
-        this.one = n;
+        this.one += n;
     }
 
     public void addFive(int n) {
-        this.five = n;
+        this.five += n;
     }
 
     public void addTen(int n) {
-        this.ten = n;
+        this.ten += n;
     }
 
     public double getMeasure() {
-        if(size < one + five + ten) {
-            if (one == size) {
-                return one * 1;
-            }else if(one < size && one + five >= size) {
-                int minusFiveSize = (one + five) - size;
-                return (one * 1) + (minusFiveSize * 5);
-            }else{
-                int minusTenSize = (one + five + ten) - size;
-                return (one * 1) + (five * 5) + (minusTenSize + 10);
-            }
-        }else{
-            return (one * 1) + (five * 5) + (ten * 10);
-        }
+        return one + five + ten;
     }
     public PiggyBank(int size) {
         this.size = size;
